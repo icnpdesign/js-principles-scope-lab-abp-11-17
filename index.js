04d14fdd7c90e9d711880024ffbffecd13acda32
icnpdesign <inp@bu.edu>
2017-11-08 17:38:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(customerName) {
+var customerName = 'bob';
+var customerNameUpper = customerName.toUpperCase();
+return customerNameUpper;
+
+
+}
+
+function setBestCustomer () {
+  var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer () {
+  var bestCustomer = 'bob'
+}
+
+var leastFavoriteCustomer = 'jill'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'ben'
+ }
+
+ favoriteCustomer = 'bob'
